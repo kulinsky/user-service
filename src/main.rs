5be03f96cee9155b86959bf65aeb4c52c3e84523
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2022-08-10 17:27:26 +0300

    try to use store interface

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ async fn main() -> Result<(), error::Error> {
 
     let tx = user_service.repository.begin().await?;
 
-    user_service.repository.rollback(tx).await;
+    user_service.repository.rollback(tx).await?;
 
     Ok(())
 }
