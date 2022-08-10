c88b83933f92273ca4ca8b61d2af295d0d8bf300
a.kulinskiy <a.kulinskiy@tinkoff.ru>
2022-08-10 17:36:05 +0300

    refact main

--- src/main.rs
+++ src/main.rs
@@ -29,20 +29,9 @@ async fn main() -> Result<(), error::Error> {
 
     let repo = implementation::postgres::repository::UserPGRepository::new(db_pool);
 
-    let test_user_id: UserID = uuid::Uuid::from_str("01827df4-cedd-7532-b110-03d430d3556f")?.into();
+    let test_user_id = uuid::Uuid::from_str("01827df4-cedd-7532-b110-03d430d3556f")?.into();
 
     let id_provider = implementation::uuid7_id_provider::UserIDProviderUUID7::new();
-
-    let test_u = User {
-        email: Email::new("test_email@email.org".to_string())?,
-        id: id_provider.provide().unwrap(),
-        first_name: "John".to_string(),
-        last_name: "Doe".to_string(),
-    };
-
-    // let _ = repo.create(&test_u).await?;
-
-    // println!("{:?}", test_u);
 
     let user_service = UserService::new(repo, id_provider);
 
